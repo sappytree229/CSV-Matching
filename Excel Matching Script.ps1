586efbf7ee83e21d51b00cf04df8f96298110590
sappytree229 <95254831+sappytree229@users.noreply.github.com>
2022-07-22 14:11:15 -0500

    Add files via upload

---
+++ Excel Matching Script.ps1
@@ -0,0 +1,24 @@
+﻿$ExcelFile = '.\Calabrio Clean Up- Finished.xlsx'
+$ExcelSheet = 'Calabrio'
+$CSVFile = '.\Users.csv'
+
+$ImportedExcelFile = Import-Excel -path $ExcelFile -WorksheetName $ExcelSheet | Where-Object `
+{ $_."Employee ID" -like $null `
+        -and $_."Deactivated" -like $null}
+
+$ImportedCSVFile = Import-Csv -Path $CSVFile
+
+foreach ($User in $ImportedExcelFile) {
+    foreach ($Data in $ImportedCSVFile) {
+        If($User."Login" -eq $Data."SAMAccountName") {
+            If($User."Email" -like $null){
+                Write-Host $Data.mail
+            }
+            Else{   Write-Host $User."Email"}
+        }
+        elseif ($User."Email" -eq $Data."mail") {
+            Write-Host $User."Email"
+        }
+    }
+
+}
